c52bc8d18a12cf367d35c46cc7d1d1f1bb4391e3
idreamshen <idream.shen@gmail.com>
2025-12-30 07:33:43 +0000

    feat: add ivy

--- init.el
+++ init.el
@@ -9,7 +9,16 @@
 
 (use-package magit)
 
+(use-package ivy
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  :bind
+  (("C-c C-r" . ivy-resume)))
+
 (use-package counsel
+  :after ivy
   :bind (("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
          ("C-c f" . counsel-git)
