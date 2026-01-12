e50a36a21c63069d748657128879e23314c00601
idreamshen <idream.shen@gmail.com>
2026-01-12 17:39:55 +0800

    feat: Add new Emacs packages for agent-shell and acp

--- flake.nix
+++ flake.nix
@@ -21,6 +21,43 @@
             config.allowUnfree = true;
             overlays = [
               (import emacs-overlay)
+              (final: prev: {
+                emacsPackagesFor = emacs: (prev.emacsPackagesFor emacs).overrideScope (self: super: {
+                  agent-shell = self.trivialBuild {
+                    pname = "agent-shell";
+                    version = "0.29.1-git";
+                    src = final.fetchFromGitHub {
+                      owner = "xenodium";
+                      repo = "agent-shell";
+                      rev = "1f60199b34eac48fe31f4c8839c5b0cef933e07e";
+                      sha256 = "1fr7vgfkaalm0zfa79l32m164afwcdm6jh6b0nrddd9ilzsaxhmi";
+                    };
+                    packageRequires = with self; [ shell-maker acp ];
+                  };
+                  shell-maker = self.trivialBuild {
+                    pname = "shell-maker";
+                    version = "0.84.4-git";
+                    src = final.fetchFromGitHub {
+                      owner = "xenodium";
+                      repo = "shell-maker";
+                      rev = "26fd77a01ca15b441fd13186cb89f6b845a08e7c";
+                      sha256 = "0x10qjhcym4nnv5zh4nwy7d46lp158q06w2iwgi265nl1bz9s7yq";
+                    };
+                    packageRequires = with self; [ ];
+                  };
+                  acp = self.trivialBuild {
+                    pname = "acp";
+                    version = "0.8.2-git";
+                    src = final.fetchFromGitHub {
+                      owner = "xenodium";
+                      repo = "acp.el";
+                      rev = "7b67facc657a7388a53ea8bba5d6e7eba20fa3e0";
+                      sha256 = "0znm5qihx2qy3hgw0idg8j7bnhz8k3yaadff3y6696qckdh0qlnr";
+                    };
+                    packageRequires = with self; [ ];
+                  };
+                });
+              })
             ];
           };
         in
