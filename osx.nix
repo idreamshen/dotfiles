c7331e283ec5870cce216a869451ee3debbca9b3
idreamshen <idream.shen@gmail.com>
2026-01-22 11:09:13 +0800

    feat: Add Maccy support to macOS configuration

--- osx.nix
+++ osx.nix
@@ -6,5 +6,15 @@ in {
   home.packages = lib.mkIf stdenv.isDarwin [
     pkgs.iterm2
     pkgs.flameshot
+    pkgs.maccy
   ];
+
+  launchd.agents.maccy = lib.mkIf stdenv.isDarwin {
+    enable = true;
+    config = {
+      ProgramArguments = [ "${pkgs.maccy}/Applications/Maccy.app/Contents/MacOS/Maccy" ];
+      RunAtLoad = true;
+      KeepAlive = false;
+    };
+  };
 }
