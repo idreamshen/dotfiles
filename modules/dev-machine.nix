d7372c4a1ee51801eb0053792cf618bdace5e392
idreamshen <idream.shen@gmail.com>
2026-05-08 10:56:00 +0800

    feat: Add it2ul utility and iTerm2 shell integration support

--- modules/dev-machine.nix
+++ modules/dev-machine.nix
@@ -1,8 +1,29 @@
 { config, pkgs, llmAgents, ... }:
 
 let
-  inherit (pkgs) lib stdenv;
   llmAgentsPkgs = llmAgents.packages.${pkgs.system};
+  it2ul = pkgs.stdenvNoCC.mkDerivation {
+    pname = "it2ul";
+    version = "unstable-2026-05-08";
+
+    src = pkgs.fetchurl {
+      url = "https://iterm2.com/utilities/it2ul";
+      sha256 = "08m9l7rkacm9kyalp4w0f5ppbiafq9jqj6q1rkd1di2jhx8h49m7";
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p "$out/bin"
+      cp "$src" "$out/bin/it2ul"
+      chmod +x "$out/bin/it2ul"
+      patchShebangs "$out/bin/it2ul"
+
+      runHook postInstall
+    '';
+  };
 in {
   imports = [
     ./emacs.nix
@@ -15,6 +36,7 @@ in {
       bun
       dart
       noto-fonts-cjk-sans
+      it2ul
       yaml-language-server
       fvm
     ])
--- modules/osx.nix
+++ modules/osx.nix
@@ -7,11 +7,15 @@ in {
     pkgs.iterm2
   ];
 
+  home.file.".iterm2_shell_integration.zsh" = lib.mkIf stdenv.isDarwin {
+    source = "${pkgs.iterm2}/Applications/iTerm2.app/Contents/Resources/iterm2_shell_integration.zsh";
+  };
+
   programs.zsh.initContent = lib.mkIf stdenv.isDarwin (lib.mkAfter ''
     # iTerm2 shell integration
     if [ "$TERM_PROGRAM" = "iTerm.app" ]; then
       export PATH="${pkgs.iterm2}/Applications/iTerm2.app/Contents/Resources:$PATH"
-      source "${pkgs.iterm2}/Applications/iTerm2.app/Contents/Resources/iterm2_shell_integration.zsh"
+      source "$HOME/.iterm2_shell_integration.zsh"
     fi
   '');
 }
