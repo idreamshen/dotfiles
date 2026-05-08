d532c2071e0c38d1495031ac646a92b386776aac
idreamshen <idream.shen@gmail.com>
2026-05-08 10:40:15 +0800

    feat: Add iTerm2 shell integration for Zsh on macOS

--- modules/osx.nix
+++ modules/osx.nix
@@ -6,4 +6,12 @@ in {
   home.packages = lib.mkIf stdenv.isDarwin [
     pkgs.iterm2
   ];
+
+  programs.zsh.initContent = lib.mkIf stdenv.isDarwin (lib.mkAfter ''
+    # iTerm2 shell integration
+    if [ "$TERM_PROGRAM" = "iTerm.app" ]; then
+      export PATH="${pkgs.iterm2}/Applications/iTerm2.app/Contents/Resources:$PATH"
+      source "${pkgs.iterm2}/Applications/iTerm2.app/Contents/Resources/iterm2_shell_integration.zsh"
+    fi
+  '');
 }
