1a651ceeb959b249a2d737615c0fb82c4960adec
idreamshen <idream.shen@gmail.com>
2026-05-17 00:15:01 +0000

    chore: Update Nix substituter mirror URL

--- modules/common.nix
+++ modules/common.nix
@@ -76,7 +76,7 @@ in {
   nix.package = pkgs.nix;
   nix.settings = {
     substituters = [
-      "https://mirrors.cernet.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
     ];
     trusted-public-keys = [
